c495f67cf72c3154c619696a7bdc733c3142f31d
Samantha Lima Luna Oriá <samanthalimaadv@yahoo.com.br>
2020-11-12 15:42:03 +0100

    ignoring a package

---
+++ using-require.js
@@ -0,0 +1,8 @@
+const toTitleCase = require("to-title-case");
+const toTittleCase = require("to-title-case");
+console.log(toTittleCase);
+
+const lower = "The lord of the rings: the return of the king";
+const tittle = toTitleCase(lower);
+
+console.log(lower);
